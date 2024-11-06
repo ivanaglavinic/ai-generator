01291e9ac6418b8122377435bf51d8d2fca7337c
ivanaglavinic <ivana.glavinic8@gmail.com>
2024-11-06 20:26:43 +0100

    Added API integration

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-function generateResult(event) {
-  event.preventDefault();
-
+function displayResult(response) {
+  console.log("Result generated");
   new Typewriter("#result", {
-    strings: "The result will be here",
+    strings: response.data.answer,
     autoStart: true,
     cursor: null,
-    delay: 30,
+    delay: 15,
   });
+}
+
+function generateResult(event) {
+  event.preventDefault();
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "2be12f1a1ef003988503e7odt2f49b94";
+  let prompt = `Generate only one short and structured quick meal recipe about ${instructionsInput.value}`;
+  let context =
+    "You are an expert in quick meal recipes, you love cooking food from cultures all over the world. Your mission is to write short recipes based on ingredients from user instructions. Write recipe in basic HTML without word html being displayed and make sure that each step is documented in new text line with <br/>. ";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating result");
+
+  axios.get(apiUrl).then(displayResult);
 }
 
 let formElement = document.querySelector("#form");
