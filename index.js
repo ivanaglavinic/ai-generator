736c7ee6323177003cc7b6afb84711a1b0913492
ivanaglavinic <ivana.glavinic8@gmail.com>
2024-11-06 21:20:52 +0100

    Generated API

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 function displayResult(response) {
-  console.log("Result generated");
   new Typewriter("#result", {
     strings: response.data.answer,
     autoStart: true,
@@ -14,11 +13,12 @@ function generateResult(event) {
   let apiKey = "2be12f1a1ef003988503e7odt2f49b94";
   let prompt = `Generate only one short and structured quick meal recipe about ${instructionsInput.value}`;
   let context =
-    "You are an expert in quick meal recipes, you love cooking food from cultures all over the world. Your mission is to write short recipes based on ingredients from user instructions. Write recipe in basic HTML without word html being displayed and make sure that each step is documented in new text line with <br/>. ";
+    "You are an expert in quick meal recipes, you love cooking food from cultures all over the world. Your mission is to write very creative short recipes based on ingredients from user instructions. Write recipe in basic html format, WITHOUT WORD HTML BEING DISPLAYED and make sure that each step is documented in new text line with <br/>. Make sure that you generate new idea recipe every time user enters submit answer ";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  console.log("Generating result");
-
+  resultElement = document.querySelector("#result");
+  resultElement.style.display = "block";
+  resultElement.innerHTML = `<div class = "blink"> Your recipe is on the way... </div>`;
   axios.get(apiUrl).then(displayResult);
 }
 
